eb8590a38243ea755dbe039d6f95890e673df1d2
Damian <damianmarut1@gmail.com>
2018-06-29 12:50:06 +0200

    port update in app.js

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/users", userRoutes);
 
-app.listen(3000 || process.env.PORT, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!");
 });
